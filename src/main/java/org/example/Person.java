c5fa0b908579ae1f2099e47e53b65c5a174d3628
asrytikov <asrytikov@gmail.com>
2023-11-17 19:29:40 +0400

    Lesson 30

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,23 +10,23 @@ public class Person {
     private String name = "Alex";
 
     //@Autowired
-    //private final Dog dog;
+    private final Dog dog;
 
-    private Dog dog;
+    //private Dog dog;
 
     //@Autowired
-    public Person(Dog dog) {
-        this.dog = dog;
+   public Person(Dog dog2) {
+        this.dog = dog2;
     }
 
     public Dog getDog() {
         return dog;
     }
 
-    @Autowired
-    public void setDog(Dog dog) {
+    //@Autowired
+    /*public void setDog(Dog dog) {
         this.dog = dog;
-    }
+    }*/
 
     public String getName() {
         return name;
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,4 +8,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
+
+    /*@Bean
+    public Person person(@Qualifier("dog2") Dog dog){
+        Person person = new Person();
+        person.setName("Alex");
+        person.setDog(dog);
+        return person;
+    }*/
+
+    /*@Bean
+    public Person person(Dog dog2){
+        Person person = new Person();
+        person.setName("Alex");
+        person.setDog(dog2);
+        return person;
+    }*/
+
+    @Bean
+    public Dog dog1(){
+        Dog dog = new Dog();
+        dog.setName("Reks");
+        return dog;
+    }
+
+    @Bean
+    public Dog dog2(){
+        Dog dog = new Dog();
+        dog.setName("Sharik");
+        return dog;
+    }
 }
